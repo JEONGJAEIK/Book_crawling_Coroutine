88b1aa3c63e6136e160b1eb834521ab091cdcdf7
JEONGJAEIK <wodlr1207@naver.com>
2025-02-22 19:57:15 +0900

    크롤링 리팩토링

--- src/main/kotlin/com/example/demo/scheduler/CrawlingScheduler.kt
+++ src/main/kotlin/com/example/demo/scheduler/CrawlingScheduler.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component
 @Component
 class CrawlingScheduler(private val crawlingService: CrawlingService) {
 
-    @Scheduled(cron = "0 22 * * * *")
+    @Scheduled(cron = "0 10 * * * *")
     fun scheduledCrawling() {
         println("크롤링 시작: ${java.time.LocalDateTime.now()}")
         crawlingService.crawling()
--- src/main/kotlin/com/example/demo/service/CrawlingService.kt
+++ src/main/kotlin/com/example/demo/service/CrawlingService.kt
@@ -1,8 +1,9 @@
 package com.example.demo.service
 
 import com.example.demo.dto.BookDTO
+import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
-import com.microsoft.playwright.options.LoadState
+import com.microsoft.playwright.PlaywrightException
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
@@ -11,71 +12,76 @@ class CrawlingService(private val bookService: BookService) {
 
     @Transactional
     fun crawling() {
-        val playwright = Playwright.create()
-        val browser = playwright.chromium().launch()
-        val page = browser.newPage()
+        Playwright.create().use { playwright ->  // playwright 객체 생성
+            val browser = playwright.chromium().launch() //크롬 브라우저 실행
+            val page = browser.newPage() // 새 브라우저 탭 열기
 
-        val bookLinks = mutableListOf<String>()
+            val bookLinks = mutableListOf<String>() // 링크 리스트 생성
 
-        for (bestsellerPage in 1..2) {
-            page.navigate("https://store.kyobobook.co.kr/bestseller/realtime?page=$bestsellerPage&per=50")
-            page.waitForLoadState(LoadState.DOMCONTENTLOADED)
-            page.waitForSelector("div.ml-4 > .prod_link")
+            for (bestsellerPage in 1..2) {
+                page.navigate("https://store.kyobobook.co.kr/bestseller/realtime?page=$bestsellerPage&per=50") // 교보문고 접속
+                page.waitForSelector("div.ml-4 > .prod_link") // 해당 요소가 추가된 후 실행
 
-            val links = page.locator("div.ml-4 > .prod_link").all()
-            links.forEach { link ->
-                link.getAttribute("href")?.let { href ->
-                    bookLinks.add(href)
+                val links = page.locator("div.ml-4 > .prod_link").all() // 해당 요소를 추가하고 list<Locator>로 반환
+                bookLinks.addAll(links.mapNotNull { it.getAttribute("href") }) // 해당 요소에서 링크만 추출해서 list<String>으로 반환
+            }
+
+            println("총 ${bookLinks.size}개의 도서 링크 수집 완료!")
+
+            val bestsellers = mutableListOf<BookDTO>()
+
+            bookLinks.forEachIndexed { ranking, bookUrl -> //랭킹과 url 반복
+                page.navigate(bookUrl)                     // url 페이지 열기
+
+                val title = getTextOrEmpty(page, ".prod_title")
+                val author = getTextOrEmpty(page, ".author")
+                val isbn = getTextOrEmpty(page, "#scrollSpyProdInfo > div.product_detail_area.basic_info > div.tbl_row_wrap > table > tbody > tr:nth-child(1) > td")
+                val description = getTextOrEmpty(page, ".intro_bottom")
+                val image = getAttributeOrEmpty(page, ".portrait_img_box img", "src")
+
+                if (title.isNotBlank() || author.isNotBlank() || isbn.isNotBlank() || description.isNotBlank() || image.isNotBlank()) {
+                    bestsellers.add(
+                        BookDTO(
+                            id = 0L,
+                            title = title,
+                            author = author,
+                            description = description,
+                            image = image,
+                            isbn = isbn,
+                            ranking = ranking + 1,
+                            favoriteCount = 0
+                        )
+                    )
                 }
             }
+            println("총 ${bestsellers.size}개의 도서 데이터 저장 완료!")
+            bookService.saveBestsellers(bestsellers)
         }
+    }
 
-        val bestsellers = mutableListOf<BookDTO>()
-
-        bookLinks.forEachIndexed { ranking, bookUrl ->
-            page.navigate(bookUrl)
-            page.waitForLoadState(LoadState.DOMCONTENTLOADED)
-
-            val title = getTextOrEmpty(page, ".prod_title")
-            val author = getTextOrEmpty(page, ".author")
-            val isbn = getTextOrEmpty(
-                page,
-                "#scrollSpyProdInfo > div.product_detail_area.basic_info > div.tbl_row_wrap > table > tbody > tr:nth-child(1) > td"
-            )
-            val description = getTextOrEmpty(page, ".intro_bottom")
-            val image = getAttributeOrEmpty(page, ".portrait_img_box img", "src")
-            if (title.isNotBlank() || author.isNotBlank() || isbn.isNotBlank() || description.isNotBlank() || image.isNotBlank()) {
-                val book = BookDTO(
-                    id = 0L,
-                    title = title,
-                    author = author,
-                    description = description,
-                    image = image,
-                    isbn = isbn,
-                    ranking = ranking + 1,
-                    favoriteCount = 0
-                )
-                bestsellers.add(book)
+    fun getTextOrEmpty(page: Page, selector: String): String {
+        return try {
+            val element = page.waitForSelector(selector, Page.WaitForSelectorOptions().setTimeout(3000.0))
+            element?.textContent()?.trim()?.replace("\n", "") ?: ""
+        } catch (e: PlaywrightException) {
+            if (e.message?.contains("Timeout") == true) {
+                ""
+            } else {
+                throw e
             }
         }
-        browser.close()
-        playwright.close()
-        bookService.saveBestsellers(bestsellers)
     }
 
-    fun getTextOrEmpty(page: com.microsoft.playwright.Page, selector: String): String {
-        return if (page.locator(selector).count() > 0) {
-            page.locator(selector).first().textContent()?.trim()?.replace("\n", " ") ?: ""
-        } else {
-            ""
-        }
-    }
-
-    fun getAttributeOrEmpty(page: com.microsoft.playwright.Page, selector: String, attribute: String): String {
-        return if (page.locator(selector).count() > 0) {
-            page.locator(selector).first().getAttribute(attribute)?.trim()?.replace("\n", " ") ?: ""
-        } else {
-            ""
+    fun getAttributeOrEmpty(page: Page, selector: String, attribute: String): String {
+        return try {
+            val element = page.waitForSelector(selector, Page.WaitForSelectorOptions().setTimeout(3000.0))
+            element?.getAttribute(attribute)?.trim()?.replace("\n", "") ?: ""
+        } catch (e: PlaywrightException) {
+            if (e.message?.contains("Timeout") == true) {
+                ""
+            } else {
+                throw e
+            }
         }
     }
 }
